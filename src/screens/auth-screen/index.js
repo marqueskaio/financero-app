6cdb09967b1c7bf2379b480b4ddd573764f1adc0
João Victor Equer <joaovictorequer@gmail.com>
2024-01-14 20:08:34 -0300

    Screen auth styles

--- src/screens/auth-screen/index.js
+++ src/screens/auth-screen/index.js
@@ -1,12 +1,21 @@
-import {Text} from "react-native";
 import {ContainerComponent} from "../../hoc/container-component";
-
+import {View, Text, TextInput, Button, Image} from "react-native";
 export const AuthScreen = () => {
   return (
     <ContainerComponent>
-      <Text style={{margin: 100}}>Login</Text>
+        <View style={{backgroundColor:"orange", width:"100%", height:"40%", flexDirection:"row", justifyContent: "center", alignItems:"center" }}>
+            <Image source={require("../../assets/Logo.png")} style={{width:100,height:100, borderRadius:50, }}/>
+            <Text style={{fontWeight:"bold", color:"white", fontSize:60}}>Financero</Text>
+        </View>
+        <View style={{paddingHorizontal:100,paddingVertical:100}}>
+            <Text style={{fontWeight:"bold", alignItems:"flex-start"}}>E-mail:</Text>
+            <TextInput style={{backgroundColor:"rgba(0,0,0,0.1)", borderRadius:4, marginBottom:8}} placeholder={"Digite seu E-mail"}/>
+            <Text style={{fontWeight:"bold", alignItems:"flex-start"}}>Senha:</Text>
+            <TextInput style={{backgroundColor:"rgba(0,0,0,0.1)", borderRadius:4,marginBottom:32}}placeholder={"Digite sua Senha"}/>
+            <Button title={"Entrar"} color={"orange"}/>
+            <Text style={{paddingVertical:20, textAlign:"center"}}>Ainda não possui conta?</Text>
+            <Button title={"Criar Conta"} color={"orange"}/>
+        </View>
     </ContainerComponent>
-    // <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-    // </View>
   )
 }
--- src/screens/home-screen/index.js
+++ src/screens/home-screen/index.js
@@ -4,7 +4,7 @@ import {View, Text} from "react-native";
 export const HomeScreen = () => {
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Login</Text>
+      <Text>login</Text>
     </View>
   )
 }
