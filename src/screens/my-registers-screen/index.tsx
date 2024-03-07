d8652a1d69da16649be5f23b4786f21c5bd28aec
João Victor Equer <joaovictorequer@gmail.com>
2024-03-07 11:22:17 -0300

    My-Registers-Screen

--- src/screens/my-registers-screen/index.tsx
+++ src/screens/my-registers-screen/index.tsx
@@ -1,8 +1,17 @@
 import {AccountHeaderComponent} from "../../components/account-header-component";
+import {View, Text} from "react-native";
+import React from "react";
 
 export const MyRegistersScreen = () => {
 
     return(
+        <>
         <AccountHeaderComponent/>
+        <View>
+            <Text style={{fontSize: 32, fontWeight: "bold", color:"#f27c22", alignSelf:"center"}}>
+                Meus Registros
+            </Text>
+        </View>
+        </>
     )
 }
