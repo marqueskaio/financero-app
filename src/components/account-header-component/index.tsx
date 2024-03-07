812aa8a28da3b8b47f012af0b0e69514a55b0451
ayrtonlima7 <ayrtonlima1997@gmail.com>
2024-03-07 12:49:31 -0300

    LeftArrowCircleIcon

--- src/components/account-header-component/index.tsx
+++ src/components/account-header-component/index.tsx
@@ -1,26 +1,41 @@
-import {Text, View} from "react-native";
-import {LogoIcon} from "../../assets/icon";
+import { Text, View } from "react-native";
+import { LeftArrowCircleIcon, LogoIcon } from "../../assets/icon";
 import React from "react";
-import {LinearGradient} from "react-native-linear-gradient";
+import { LinearGradient } from "react-native-linear-gradient";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
-export const AccountHeaderComponent = () => {
+interface AccountHeaderProps {
+    onPress: () => void
+}
+
+export const AccountHeaderComponent = ({ onPress }: AccountHeaderProps) => {
     return (
-            <View style={{backgroundColor:"#f27c22", width:"100%", height: "21%", padding: 28,}}>
-                <View style={{flexDirection: "row", width: "100%"}}>
-                    <View style={{width: "50%"}}>
-                        <Text style={{color: "white", fontSize: 28, fontWeight: "bold"}}>Olá, Robertinho</Text>
+        <View style={{ backgroundColor: "#f27c22", width: "100%", height: "21%", padding: 28, }}>
+            <View style={{ flexDirection: "row", width: "100%" }}>
+                {typeof onPress === "function" ? 
+                    (
+                    <TouchableOpacity onPress={onPress}>
+                        <LeftArrowCircleIcon width={30} height={30}/>
+                    </TouchableOpacity>
+                    )
+                    : 
+                    (
+                    <View style={{ width: "50%" }}>
+                        <Text style={{ color: "white", fontSize: 28, fontWeight: "bold" }}>Olá, Robertinho</Text>
                     </View>
-                    <View style={{alignItems: "flex-end", width: "50%"}}>
-                        <View style={{backgroundColor: "red"}}>
-                            <LogoIcon width={48} height={48}/>
-                        </View>
+                    )
+                }
+                <View style={{ alignItems: "flex-end", width: "50%" }}>
+                    <View style={{ backgroundColor: "red" }}>
+                        <LogoIcon width={48} height={48} />
                     </View>
+                </View>
 
-                </View>
-                <View>
-                    <Text style={{color: "white", fontSize: 22, fontWeight: "bold"}}>Conta</Text>
-                    <Text style={{color: "white", fontSize: 22}}>R$8.998,00</Text>
-                </View>
             </View>
+            <View>
+                <Text style={{ color: "white", fontSize: 22, fontWeight: "bold" }}>Conta</Text>
+                <Text style={{ color: "white", fontSize: 22 }}>R$8.998,00</Text>
+            </View>
+        </View>
     )
 }
--- src/screens/my-registers-screen/index.tsx
+++ src/screens/my-registers-screen/index.tsx
@@ -1,12 +1,14 @@
 import {AccountHeaderComponent} from "../../components/account-header-component";
 import {View, Text} from "react-native";
 import React from "react";
+import { useNavigation } from "@react-navigation/native";
 
 export const MyRegistersScreen = () => {
+    const navigation = useNavigation<any>()
 
     return(
         <>
-        <AccountHeaderComponent/>
+        <AccountHeaderComponent onPress={() => navigation.navigate("HomeScreen")}/>
         <View>
             <Text style={{fontSize: 32, fontWeight: "bold", color:"#f27c22", alignSelf:"center"}}>
                 Meus Registros
